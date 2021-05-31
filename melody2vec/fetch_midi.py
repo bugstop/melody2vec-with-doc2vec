709ce831dddb3bbe7ee9700b6cefb59a0b696884
Muhan Li <github@umm.one>
2021-06-01 01:36:48 +0800

    similarity

--- melody2vec/fetch_midi.py
+++ melody2vec/fetch_midi.py
@@ -3,9 +3,13 @@ from shutil import copyfile
 
 with open('melody.json') as f:
     melody = json.load(f)
+while True:
+    x = input('-> ')
+    if not x:
+        break
+    index = int(x)
 
-for index in [0, 245, 9533]:
     filename = melody[index][-1]
     input('copy ' + filename + ' -> ')
     midi = './midi/' + f"{filename[0]}/"
-    copyfile(midi + filename, f'z{index:05d}_' + filename)
+    copyfile(midi + filename, f'melody_{index:05d}_' + filename)
--- melody2vec/inference.py
+++ melody2vec/inference.py
@@ -10,20 +10,21 @@ model = gensim.models.Doc2Vec.load('d2v.bin')
 alpha = 0.01
 epoch = 1000
 
-sims = model.docvecs.most_similar([model.docvecs[0]], topn=10)
-print(sims)
+while True:
+    x = input('-> ')
+    if not x:
+        break
+    x = int(x)
 
-inferred = model.infer_vector(melody[0][1].split(' '), alpha=alpha, steps=epoch)
-# print(type(inferred), inferred.tolist())
-sims = model.docvecs.most_similar([inferred], topn=10)
-print(sims)
+    sims = model.docvecs.most_similar(positive=[model.docvecs[x]], topn=10)
+    print(sims)
+    sims = model.docvecs.most_similar(negative=[model.docvecs[x]], topn=10)
+    print(sims)
 
-print(melody[0][-1], melody[245][-1], melody[9533][-1])
+    inferred = model.infer_vector(melody[x][1].split(' '), alpha=alpha, steps=epoch)
+    # print(type(inferred), inferred.tolist())
 
-# [(0, 1.0), (8811, 0.9978615045547485), (8199, 0.9833886027336121), (245, 0.9817537665367126),
-#     (9533, 0.7279029488563538), (6413, 0.6909092664718628), (2683, 0.6771758794784546),
-#     (1442, 0.6739320158958435), (5882, 0.6705861687660217), (4714, 0.6636027097702026)]
-# [(8811, 0.9855718612670898), (0, 0.983768880367279), (8199, 0.9715628623962402), (245, 0.970552384853363),
-#     (9533, 0.7039425373077393), (6413, 0.645868182182312), (5884, 0.6291266083717346),
-#     (5882, 0.6259562969207764), (4714, 0.6193612217903137), (4073, 0.6068050861358643)]
-# 000e12872af8de95ab2f3b4957da5805.mid 055df847de7ea214a5f57b177e362d22.mid e31b00bc057683065e307eef3fe07928.mid
+    sims = model.docvecs.most_similar(positive=[inferred], topn=10)
+    print(sims)
+    sims = model.docvecs.most_similar(negative=[inferred], topn=10)
+    print(sims)
